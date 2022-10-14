c1dfbd7ac8ba13494c98f1c5bbe4d7a21750e0ba
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2022-10-14 11:48:12 -0400

    works

--- flask_install.sh
+++ flask_install.sh
@@ -1,7 +1,15 @@
+value=$(<dns_adress.txt)
+for str in ${value[@]}; do
+    ssh -i labsuser.pem ubuntu@$str
+done
+
+
+ssh -i labsuser.pem ubuntu@ec2-3-226-235-167.compute-1.amazonaws.com
+
+
+sudo apt-get update && sudo apt-get -y install python3-pip && sudo pip3 install flask &&
 mkdir flask_application && cd flask_application
-python3 -m venv venv
-source venv/bin/activate
 
-pip install Flask
 
-python -m flask --version
+pip3 install Flask
+
